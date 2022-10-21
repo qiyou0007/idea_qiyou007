4a8a6671b16ef37fb792bf455a2abb744251d78c
七友 <qiyou007@gmail.com>
2022-10-21 21:27:31 +0800

    七友008第二次修改

--- src/main/java/com/zld/git/HelloGit.java
+++ src/main/java/com/zld/git/HelloGit.java
@@ -6,5 +6,6 @@ public class HelloGit {
         System.out.println("第一次修改");
         System.out.println("第二次修改");
         System.out.println("qiyou008修改了的");
+        System.out.println("qiyou008第二次修改");
     }
 }
